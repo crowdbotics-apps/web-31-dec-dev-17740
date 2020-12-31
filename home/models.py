4cb49078b4a0808c86ca7c78be2ac2a68b760fc7
Crowdbotics <team@crowdbotics.com>
2020-12-31 08:03:29 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -50,6 +50,17 @@ class CustomText(models.Model):
     def field(self):
         return "title"
 
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
 
 class HomePage(models.Model):
     """
@@ -59,6 +70,14 @@ class HomePage(models.Model):
     """
 
     body = models.TextField()
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
 
     @property
     def api(self):
